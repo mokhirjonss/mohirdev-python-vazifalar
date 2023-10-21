a42e9588d8ef2b9b7784cd7ca0e7b9279e37d592
mokhirjonss <mokhirjons1@icloud.com>
2023-10-21 14:24:17 +0500

    #02 HELLO WORLD!

---
+++ #02 HELLO WORLD!/main.py
@@ -0,0 +1,28 @@
+# #02 HELLO WORLD!
+# Pythonda birinchi dasturimizni yozamiz.
+# BIRINCHI DASTURIMIZ —"Hello World!"
+# Faylni nomlashda quyidagi qoidalarga amal qiling:
+# Fayl nomi qisqa, kichkina  lotin harflari bilan va eng muhimi bo'shliq (пробел) qo'ymasdan yozilgan bo'lishi kerak
+# Fayl nomi .py bilan tugashi kerak (misol uchun faylnomi.py)
+# Faylga ikki so'zdan iborat nom qo'ymoqchi bo'lsangiz so'zlar orasini tire (-) yoki pastki chiziq (_) bilan ajrating. Misol uchun: hello-world.py yoki hello_world.py
+# Fayl nomini sonlar bilan boshlamang
+# Matn muharririda quyidagi kodni yozamiz:
+
+print("Hello World!")
+# Hello World!
+
+# print() bu Pythondagi mahsus funktsiya bo'lib, () ichida berilgan matn yoki ifodalarni ekranga (konsolga) chiqarish vazifasini bajaradi.
+# Konsolda Hello World! yozuvini ko'rgan bo'lsangiz, TABRIKLAYMIZ! Siz Pythonda birinchi dasturingizni yozdingiz.
+# AMALIYOT
+
+print('Assalom alaykum')
+# Assalom alaykum
+print('Hayrli tong!')
+# Hayrli tong!
+print(2 + 4 * 2)
+# 10
+print(19 / 5)
+# 3.8
+print(2**4)
+# 16
+
